c787b9487a171c3f928ba91556cd5e2a57e98642
Administrator <Administrator@20120603-1325>
2013-08-06 23:01:59 +0800

    test

---
+++ jse/src/com/jse/enhance/generic/TestGithub.java
@@ -0,0 +1,14 @@
+package com.jse.enhance.generic;
+
+/**
+ * @description: TODO
+ *
+ * @createtime: 2013-8-6 下午11:01:19
+ *
+ * @author zenghua
+ *
+ * @version 1.0
+ */
+public class TestGithub {
+
+}
