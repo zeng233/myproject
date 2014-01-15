85926b52bb1fb29835fe0595d618af5a2e881106
zenghua <zenghua233@163.com>
2014-01-15 06:54:54 -0800

    测试bean的作用域

--- aspring/src/main/java/com/myspring/core/ioc/TestIoc.java
+++ aspring/src/main/java/com/myspring/core/ioc/TestIoc.java
@@ -1,10 +1,15 @@
 package com.myspring.core.ioc;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.orm.hibernate4.HibernateTransactionManager;
 import org.springframework.orm.hibernate4.LocalSessionFactoryBean;
+
+import com.myspring.model.Prototype;
+import com.myspring.model.Singleton;
 
 /**
  * @description: TODO
@@ -16,11 +21,45 @@ import org.springframework.orm.hibernate4.LocalSessionFactoryBean;
  * @version 1.0
  */
 public class TestIoc {
+	public static final Log log = LogFactory.getLog(TestIoc.class);
+	
+	ApplicationContext context = new ClassPathXmlApplicationContext("spring/application-config.xml");
+	
 	public static void main(String[] args) {
+		TestIoc t = new TestIoc();
 		
-		ApplicationContext context = new ClassPathXmlApplicationContext("spring/application-config.xml");
+		t.testCommonBean();
+		
+	}
+	
+	/**
+	 * 测试bean的作用域，判断Ioc注入bean是否一样，
+	 */
+	public void testBeanScope() {
+		Singleton s1 = context.getBean("singleton", Singleton.class);
+		Singleton s2 = context.getBean("singleton", Singleton.class);
+		//返回true
+		System.out.println("单例两个对象s1==s2: " + (s1==s2));
+		Prototype p1=context.getBean("prototype", Prototype.class);
+		Prototype p2=context.getBean("prototype", Prototype.class);
+		//返回false
+		System.out.println("原始模式两个对象p1==p2: " +(p1==p2));
+	}
+	
+	/**
+	 * bean的继承关系
+	 */
+	public void testInheritance() {
+		log.info(context.getBean("child"));
+		log.info(context.getBean("c"));
+	}
+	
+	/**
+	 * 获取一般的bean
+	 */
+	public void testCommonBean() {
 		//实现了FactoryBean接口，实用getBean方法不能直接获取对象，参考5.8（Customizing instantiation logic with a FactoryBean）
-		System.out.println(context.getBean("&sessionFactory", LocalSessionFactoryBean.class)); //不能直接获取LocalSessionFactoryBean（抛异常）
+		System.out.println(context.getBean("&sessionFactory", LocalSessionFactoryBean.class)); 
 		
 		HibernateTransactionManager t = context.getBean("transactionManager", HibernateTransactionManager.class);
 		System.out.println(t.getSessionFactory());
@@ -33,5 +72,6 @@ public class TestIoc {
 		for (int i=0; i<names.length; i++) {
 			System.out.println(names[i]);
 		}
+		
 	}
 }
---
+++ aspring/src/main/java/com/myspring/model/Singleton.java
@@ -0,0 +1,14 @@
+package com.myspring.model;
+
+/**
+ * @description: TODO
+ *
+ * @createTime: 2014年1月14日 上午12:18:39
+ *
+ * @author zenghua
+ *
+ * @version 1.0
+ */
+public class Singleton {
+
+}
