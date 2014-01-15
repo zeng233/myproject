858a8a76a4f4ffafc30b1bce75b1b2f3af8ec4f3
zenghua <zenghua233@163.com>
2014-01-15 08:47:51 -0800

    JDBC批量操作

---
+++ aspring/src/main/java/com/myspring/model/Student.java
@@ -0,0 +1,32 @@
+package com.myspring.model;
+
+/**
+ * @description: TODO
+ * 
+ * @createTime: 2014年1月15日 下午11:51:07
+ * 
+ * @author zenghua
+ * 
+ * @version 1.0
+ */
+public class Student {
+	private int id;
+	private String name;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
