5bb9e1c18d9b0658e8ad8dc4bf33c39a6baf4b08
zenghua <zenghua233@163.com>
2014-01-16 07:31:03 -0800

    Iterator与Iterable的区别

--- ajse/src/main/java/com/jse/collection/iterator/TestIterator.java
+++ ajse/src/main/java/com/jse/collection/iterator/TestIterator.java
@@ -1,7 +1,7 @@
 package com.jse.collection.iterator;
 
 /**
- * @description: TODO
+ * @description: Iterator与Iterable的区别：Iterator是为了遍历当前节点的元素，Iterable表示获取一个Iterator对象（是有状态的，返回的Iterator不一样）
  *
  * @createtime: 2013-5-24 上午11:23:09
  *
