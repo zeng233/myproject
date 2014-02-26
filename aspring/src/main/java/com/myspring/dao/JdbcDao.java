298edbfad3611ae7f8c4a785365bea117bc73705
zenghua <zenghua233@163.com>
2014-02-26 00:57:54 -0800

    jdbc批量操作

--- aspring/src/main/java/com/myspring/dao/JdbcDao.java
+++ aspring/src/main/java/com/myspring/dao/JdbcDao.java
@@ -1,5 +1,7 @@
 package com.myspring.dao;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,12 +10,14 @@ import javax.sql.DataSource;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.core.namedparam.SqlParameterSourceUtils;
 import org.springframework.stereotype.Repository;
 
+import com.google.common.collect.Lists;
 import com.myspring.model.Student;
 
 /**
@@ -27,17 +31,20 @@ import com.myspring.model.Student;
  */
 @Repository
 public class JdbcDao {
-	private Log log = LogFactory.getLog(JdbcDao.class);
+	private static final Log log = LogFactory.getLog(JdbcDao.class);
+
+	private static final String QUERY_SAVE = "insert into student(id, name) values(null, :name)";
+
 	private JdbcTemplate jdbcTemplate;
 	private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
 	@Autowired
 	public void setDataSource(DataSource jdbcDataSource) {
 		this.jdbcTemplate = new JdbcTemplate(jdbcDataSource);
-		this.namedParameterJdbcTemplate = new NamedParameterJdbcTemplate(jdbcDataSource);
+		this.namedParameterJdbcTemplate = new NamedParameterJdbcTemplate(
+				jdbcDataSource);
 	}
 
-	
 	public void updateStudentById() {
 		jdbcTemplate.update("update student set name='ddd' where id=1");
 	}
@@ -49,17 +56,19 @@ public class JdbcDao {
 			s.setName("a" + i);
 			list.add(s);
 		}
-		
-		long beginTime = System.currentTimeMillis(); 
-		SqlParameterSource[] batch = SqlParameterSourceUtils.createBatch(list.toArray());
-		int[] updateCounts = namedParameterJdbcTemplate.batchUpdate("insert into student(id, name) values(null, :name)", batch);
+
+		long beginTime = System.currentTimeMillis();
+		SqlParameterSource[] batch = SqlParameterSourceUtils.createBatch(list
+				.toArray());
+		int[] updateCounts = namedParameterJdbcTemplate.batchUpdate(
+				"insert into student(id, name) values(null, :name)", batch);
 		log.info("批量插入共执行了：" + updateCounts.length + "行");
 		System.out.println("执行行数：" + updateCounts.length);
 		long endTime = System.currentTimeMillis();
-		log.info("执行了：" + (endTime-beginTime) + "毫秒");
-		System.out.println("批量插入共执行：" + (endTime-beginTime) + "毫秒");
+		log.info("执行了：" + (endTime - beginTime) + "毫秒");
+		System.out.println("批量插入共执行：" + (endTime - beginTime) + "毫秒");
 	}
-	
+
 	public void perInsertStudent() {
 		int count = 0;
 		List<Student> list = new ArrayList<Student>();
@@ -68,14 +77,39 @@ public class JdbcDao {
 			s.setName("b" + i);
 			list.add(s);
 		}
-		
-		long beginTime = System.currentTimeMillis(); 
+
+		long beginTime = System.currentTimeMillis();
 		for (Student t : list) {
 			count++;
-			jdbcTemplate.update("insert into student(id,name) values(null,'" + t.getName() + "')");
+			jdbcTemplate.update("insert into student(id,name) values(null,'"
+					+ t.getName() + "')");
 		}
 		long endTime = System.currentTimeMillis();
 		System.out.println("perInsertStudent插入行数：" + count);
-		System.out.println("单个插入共执行：" + (endTime-beginTime) + "毫秒");
-	} 
+		System.out.println("单个插入共执行：" + (endTime - beginTime) + "毫秒");
+	}
+
+	public void saveBatch(final List<Student> employeeList) {
+		final int batchSize = 500;
+		List<List<Student>> batchLists = Lists.partition(employeeList,
+				batchSize);
+		
+		//为什么要用final
+		for (final List<Student> batch : batchLists) {
+			jdbcTemplate.batchUpdate(QUERY_SAVE,
+					new BatchPreparedStatementSetter() {
+						@Override
+						public void setValues(PreparedStatement ps, int i)
+								throws SQLException {
+							Student student = batch.get(i);
+							ps.setString(1, student.getName());
+						}
+
+						@Override
+						public int getBatchSize() {
+							return batch.size();
+						}
+					});
+		}
+	}
 }
