309f0195094b69c9fbc297e8b0f8d83268df6391
zenghua <zenghua233@163.com>
2014-02-28 06:21:56 -0800

    渠道测试

---
+++ achannel/src/main/java/com/achannel/json/jsonlib/TestJsonlib.java
@@ -0,0 +1,14 @@
+package com.achannel.json.jsonlib;
+
+/**
+ * @description: TODO
+ *
+ * @createTime: 2014年2月28日 下午10:12:13
+ *
+ * @author zenghua
+ *
+ * @version 1.0
+ */
+public class TestJsonlib {
+
+}
---
+++ achannel/src/test/java/com/achannel/json/jsonlib/TestJsonlibTest.java
@@ -0,0 +1,55 @@
+package com.achannel.json.jsonlib;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @description: TODO
+ *
+ * @createTime: 2014年2月28日 下午10:13:08
+ *
+ * @author zenghua
+ *
+ * @version 1.0
+ */
+public class TestJsonlibTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
