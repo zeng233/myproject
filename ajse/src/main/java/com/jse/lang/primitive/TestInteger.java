b90b57e591c9a4cdf48a7d464c1c15731d773846
zenghua <zenghua233@163.com>
2014-03-04 22:02:51 -0800

    测试integer

--- ajse/src/main/java/com/jse/lang/primitive/TestInteger.java
+++ ajse/src/main/java/com/jse/lang/primitive/TestInteger.java
@@ -13,5 +13,24 @@ public class TestInteger {
 	public static void main(String[] args) {
 		int i = 5_2;
 		System.out.println(i);
+		
+		TestInteger t = new TestInteger();
+		t.testEquals();
+	}
+	
+	/**
+	 * Interger里面有缓存
+	 */
+	public void testEquals() {
+		Integer i = 1;
+		Integer j = 1;
+		
+		Integer a = 333;
+		Integer b = 333;
+		System.out.println(i == j);
+		System.out.println(a.equals(b));
+		
+		String k = "12354";
+		System.out.println(Integer.getInteger(k));
 	}
 }
