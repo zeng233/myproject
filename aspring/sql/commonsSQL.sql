90bd4976e55e7789b00b6c275c45d47411cd7655
zenghua <zenghua233@163.com>
2014-03-06 17:47:34 -0800

    sql学习

--- aspring/sql/commonsSQL.sql
+++ aspring/sql/commonsSQL.sql
@@ -1,1 +1,10 @@
-select * from t exist
+-- exist的用法
+select * from t exist;
+
+
+-- case when用法
+
+-- 自身连接测试
+select x.* from sclass x,sclass y
+where x.cno=''101'' and x.degree>y.degree and y.sno=''9505201'' and y.cno=''101''
+order by x.degree desc
