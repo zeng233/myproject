7a9f5fa557d70ba37989b1dcd7fa9267a9fc92d7
zenghua <zenghua233@163.com>
2014-04-01 00:32:27 +0800

    map测试

--- ajse/src/main/java/com/jse/collection/map/TestInterviewMap.java
+++ ajse/src/main/java/com/jse/collection/map/TestInterviewMap.java
@@ -5,6 +5,8 @@ import java.util.Hashtable;
 import java.util.Map;
 import java.util.Map.Entry;
 
+import junit.framework.Assert;
+
 /**
  * @description: TODO
  *
@@ -26,29 +28,66 @@ public class TestInterviewMap {
 	 * HashMap是如何工作的
 	 */
 	public void testHashMap() {
-		Map<String,Object> map = new HashMap<>();
+		Map<Object,Object> map = new HashMap<>();
 		map.put(null, "a");
-//		map.put(null, "b");
-//		map.put("1", "1");
-//		map.put("1", "a");
+		map.put(null, "b");
+		map.put("1", "1");
+		map.put("1", "a");//key相同覆盖前面的内容
 		
-		for (Entry<String,Object> entry : map.entrySet()) {
-			String key = entry.getKey();
+		for (Entry<Object,Object> entry : map.entrySet()) {
+			Object key = entry.getKey();
 			Object value = entry.getValue();
 			System.out.println("key: " + key + ", " + "value:" + value);
 		}
-	}
-	
-	/**
-	 * hashmap与hashtable的区别（参考hashmap中文api）
-	 */
-	public void testHashMapVsHashTable() {
+		
+		Map<Person,String> map1 = new HashMap<>();
+		Person p = new Person();
+		p.setId(1);
+		p.setName("person");
+		map1.put(p, p.getName());
+		
+		Assert.assertEquals(p.getName(), "person");
+		p.setName("pp");
+		for (Person person : map1.keySet()) {
+//			Assert.assertEquals(person.equals(p), true);
+//			Assert.assertEquals(person.hashCode(), p.hashCode());
+			System.out.println(person.equals(p));
+			System.out.println(person.hashCode()==p.hashCode());
+		}
+		System.out.println(map1.get(p).equals("pp"));//map的可以不可变
+		
+		//hashmap与hashtable的区别（参考hashmap中文api）
 		Map<String, Object> table = new Hashtable<>();
 		//key如果为空，直接抛空指针异常
-		table.put(null, "1");
+//		table.put(null, "1");
+		table.put("s", "s");
 		
 		for (Entry<String, Object> entry : table.entrySet()) {
 			System.out.println(entry.getKey() + " : " + entry.getValue());
 		}
 	}
+	
+}
+
+
+class Person {
+	private int id;
+	private String name;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
 }
