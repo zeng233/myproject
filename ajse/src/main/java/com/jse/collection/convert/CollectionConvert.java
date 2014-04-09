9ac2ef4764eddfd91c7e1f97b69463737233ceae
zenghua <zenghua233@163.com>
2014-04-10 00:46:02 +0800

    集合互相转换

--- ajse/src/main/java/com/jse/collection/convert/CollectionConvert.java
+++ ajse/src/main/java/com/jse/collection/convert/CollectionConvert.java
@@ -1,5 +1,6 @@
 package com.jse.collection.convert;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
@@ -20,14 +21,25 @@ public class CollectionConvert {
 		c.convert();
 	}
 	
+	/**
+	 *  数组与List、Set、Map之间的转换
+	 */
 	public void convert() {
 		String[] array = new String[]{"a", "b", "c"};
-		//转化成List
+		//数组转化成List
 		List<String> list = Arrays.asList(array);
+//		list.add("aslist");返回的是Arrays中的list没有重写add方法，用AbstractList的add方法抛异常
 		System.out.println("list size : " + list.size());
-		//转换成Set
+		//list转换成Set
 		Set<String> set = new HashSet<>(list);
 		System.out.println("set size : " + set.size());
+		
+		//set转换成list
+		set.add("set转换成list");
+		List<String> mylist = new ArrayList<>(set);
+		for (String s : mylist) {
+			System.out.println("mylist : " + s);
+		}
 		//Map转化成List、Set
 	}
 }
