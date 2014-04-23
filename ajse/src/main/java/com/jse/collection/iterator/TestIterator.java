4a10e597f731c2cb85a3e773b36d97cd48cd3f4b
zenghua <zenghua233@163.com>
2014-04-24 01:12:20 +0800

    集合

--- ajse/src/main/java/com/jse/collection/iterator/TestIterator.java
+++ ajse/src/main/java/com/jse/collection/iterator/TestIterator.java
@@ -1,5 +1,12 @@
 package com.jse.collection.iterator;
 
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.Vector;
+
 /**
  * @description: Iterator与Iterable的区别：Iterator是为了遍历当前节点的元素，Iterable表示获取一个Iterator对象（是有状态的，返回的Iterator不一样）
  *
@@ -14,11 +21,55 @@ public class TestIterator {
 	public static void main(String[] args) {
 		TestIterator t = new TestIterator();
 		t.testMyIterator();
+		t.testIteratorRemove();
 	}
 	
 	public void testMyIterator() {
 		for (int i : new MyIterator(3)) {
-			System.out.println(i);
+//			System.out.println(i);
 		}
+	}
+	
+	public void testIteratorRemove() {
+		Set<String> set = new HashSet<>();
+		Collections.addAll(set, "a","b","c");
+		
+		for (Iterator<String> ite = set.iterator(); ite.hasNext();) {
+			String s = ite.next();
+//			ite.remove();//防止ConcurrentModificationException，该元素为Iterator逻辑上的元素，如果去掉该方法则抛异常
+			System.out.println(s);
+			
+		}
+		
+		loop(set);
+	}
+	
+	/**
+	 * iterator没有用remove方法就会抛异常
+	 * @param set
+	 */
+	public void loop(Set<String> set) {
+		for (String ss :set) {
+			set.remove(ss);
+		}
+	}
+	
+	/**
+	 * Iterator与Enumeration之间的区别
+	 */
+	public void testIteratorVsEnumeration() {
+		Vector<String> vector = new Vector<>();
+		Collections.addAll(vector, "a","b","c");
+		
+		//EnumerationJDK1.0出现
+		for(Enumeration<String> en = vector.elements(); en.hasMoreElements();) {
+			String s = en.nextElement();
+		}
+		
+		//Iterator是JDK1.2之后出现，增加了remove方法
+		for (Iterator<String> ite = vector.iterator(); ite.hasNext();) {
+			String s = ite.next();
+		}
+		
 	}
 }
--- ajse/src/main/java/com/jse/collection/set/TestSet.java
+++ ajse/src/main/java/com/jse/collection/set/TestSet.java
@@ -19,19 +19,35 @@ import java.util.Set;
 
 public class TestSet {
 	public static void main(String[] args) {
+		TestSet t = new TestSet();
+		t.testLoop();
+	}
+	
+	public void testLoop() {
 		List<String> list = new ArrayList<>();
+		list.add("d");
 		list.add("a");
 		list.add("a");
 		list.add("b");
-		//list转换为set
+		list.add("c");
+		list.add("Z");
+		list.add("A");
+		
+		//list转换为set,排除重复元素
 		Set<String> set = new HashSet<>(list);
+//		Collections.addAll(set, "e","f","g","h");
 		
 		for (String s : set) {
-//			System.out.println(s);
+			System.out.println(s);
 		}
 		
 		Collections.sort(list, String.CASE_INSENSITIVE_ORDER);
-		
+		for (String s : list) {
+			System.out.println(s);
+		}
+	}
+	
+	public void testSortByLetter() {
 		Object[] objs = {"zeng", "华", "a", "订单"};
 		System.out.println("原始：" + objs);
 		for(int i=0; i<objs.length; i++) {
