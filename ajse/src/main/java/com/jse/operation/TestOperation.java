00d6f00b3d14bd04cf7e60291a826a9110731df9
zenghua <zenghua233@163.com>
2014-04-24 14:13:38 +0800

    复合操作符陷阱

--- ajse/src/main/java/com/jse/operation/TestOperation.java
+++ ajse/src/main/java/com/jse/operation/TestOperation.java
@@ -13,7 +13,17 @@ public class TestOperation {
 	public static void main(String[] args) {
 		TestOperation t = new TestOperation();
 		System.out.println((1 << 30) *0.75);
+		
 	}
 	
-	
+	/**
+	 * +=复合操作符
+	 */
+	public void testAdd() {
+		int i = 1;
+		long j = 7;
+//		i = i+j;不能编译
+		i+=j;//编译通过，参考http://docs.oracle.com/javase/specs/jls/se8/html/jls-15.html#jls-15.26.2
+		System.out.println(i);
+	}
 }
