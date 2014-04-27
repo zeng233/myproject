f6ed6b25044aa690b90f02726fe4885b234929c3
dukezeng <zenghua233@163.com>
2014-04-28 00:36:06 +0800

    测试iterator

--- ajse/src/main/java/com/jse/collection/iterator/MyIterator.java
+++ ajse/src/main/java/com/jse/collection/iterator/MyIterator.java
@@ -5,27 +5,35 @@ import java.util.Iterator;
 /**
  * @description: TODO
  *
- * @createTime: 2014年4月16日 上午12:03:24
+ * @createTime: 2014年4月26日 下午8:34:12
  *
  * @author zenghua
  *
  * @version 1.0
  */
-public class MyIterator implements Iterable<Integer> {
-	private int count;
+public class MyIterator implements Iterator<String> {
+	private int i;
 	
 	public MyIterator(int i) {
-		this.count = i;
+		this.i = i;
 	}
 
 	@Override
-	public Iterator<Integer> iterator() {
-		return new Iterator<Integer>() {
-			private int i = 0;
-			public boolean hasNext() { return i < count; }
-			public Integer next() { i++; return i; }
-			public void remove(){ throw new UnsupportedOperationException(); }
-		};
+	public boolean hasNext() {
+		// TODO Auto-generated method stub
+		return false;
 	}
 
+	@Override
+	public String next() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void remove() {
+		// TODO Auto-generated method stub
+		
+	}
+	
 }
--- ajse/src/main/java/com/jse/collection/iterator/TestIterator.java
+++ ajse/src/main/java/com/jse/collection/iterator/TestIterator.java
@@ -1,9 +1,12 @@
 package com.jse.collection.iterator;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Enumeration;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
 import java.util.Set;
 import java.util.Vector;
 
@@ -21,14 +24,23 @@ import java.util.Vector;
 public class TestIterator {
 	public static void main(String[] args) {
 		TestIterator t = new TestIterator();
-		t.testMyIterator();
-		t.testIteratorRemove();
+//		t.testMyIterator();
+//		t.testIteratorRemove();
+		t.testIteratorVsListIterator();
 	}
 	
+	/**
+	 * 分别遍历MyIterables，MyIterator
+	 */
 	public void testMyIterator() {
-		for (int i : new MyIterator(3)) {
+		for (int i : new MyIterable(3)) {
 //			System.out.println(i);
 		}
+		
+		//增强for循环中，最有一个遍历必须是数组或者集合（实现Iterable接口），次循环编译不通过
+//		for (String s : new MyIterator(4)) {
+//			
+//		}
 	}
 	
 	/**
@@ -42,11 +54,35 @@ public class TestIterator {
 		for (Iterator<String> ite = set.iterator(); ite.hasNext();) {
 			String s = ite.next();
 //			ite.remove();//防止ConcurrentModificationException，该元素为Iterator逻辑上的元素，如果去掉该方法则抛异常
-			System.out.println(s);
+//			System.out.println(s);
 			
 		}
 		//再次遍历
-		loop(set);
+//		loop(set);
+		
+		
+		List<String> list = new ArrayList<>();
+		Collections.addAll(list, "a", "b", "c");
+		
+		Iterator<String> ite = list.iterator();
+		while(ite.hasNext()) {
+			String element = ite.next();
+			
+//			list.iterator();
+//			list.isEmpty();
+//			list.get(1);
+			//如果用Iterator迭代集合，没有使用iterator的remove方法，只要是对集合进行增加或者删除操作都会抛出ConcurrentModificationException
+			//这就是 传说中的 fail fast（参考Java Generics and Collections的11.5.2. JDK 1.2: Synchronized Collections and Fail-Fast Iterators） 
+//			list.add("d");
+//			list.removeAll(list);
+//			list.remove(element);
+		}
+		
+		//增强for循环内部也是用iterator来遍历的,如果没用使用Iterator的remove方法，同样会抛出ConcurrentModificationException
+		for (String s : list) {
+//			list.remove(s);
+		}
+		
 	}
 	
 	public void loop(Set<String> set) {
@@ -72,5 +108,26 @@ public class TestIterator {
 			String s = ite.next();
 		}
 		
+	}
+	
+	/**
+	 * Iterator与ListIterator的区别
+	 */
+	public void testIteratorVsListIterator() {
+		List<String> list = new ArrayList<>();
+		Collections.addAll(list, "a", "b", "c");
+		list.add("d");
+		//ListIterator迭代有顺序，可以指定从哪个索引开始遍历，只适用于List(可参考ListIterator API),正向迭代
+		for (ListIterator<String> ite = list.listIterator(1); ite.hasNext(); ) {
+			String s = ite.next();
+			//返回对next后续调用所返回元素的索引
+			int i = ite.nextIndex();
+			System.out.println("正向遍历 " + i +" ：" + s);
+		}
+		//反向迭代
+		for(ListIterator<String> ite = list.listIterator(list.size()); ite.hasPrevious();) {
+			String s = ite.previous();
+			System.out.println("反向遍历：" + s);
+		}
 	}
 }
