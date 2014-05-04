d134b43d407839916dbee0d366271fa02339d2c4
dukezeng <zenghua233@163.com>
2014-05-05 00:46:03 +0800

    带final的字符串疑问

--- ajse/src/main/java/com/jse/lang/TestString.java
+++ ajse/src/main/java/com/jse/lang/TestString.java
@@ -21,6 +21,9 @@ public class TestString {
 	private String name ="first";
 	private int i = 1;
 	
+	private static String as ="a";
+	private static String bs ="b";
+	
 	public static void main(String[] args) {
 		TestString t = new TestString();
 		int temp = 2;
@@ -40,11 +43,11 @@ public class TestString {
 		
 //		t.testFormat();
 		
-		t.testRex();
-//		t.testEquals();
+//		t.testRex();
+		t.testEquals();
 		
 //		t.testContains();
-		t.joinString();
+//		t.joinString();
 	}
 	
 	public void testContains() {
@@ -70,19 +73,29 @@ public class TestString {
 		System.out.println("SQL:" + sql);
 	}
 	
+	/**
+	 * 字符串创建对象
+	 * 
+	 * final解惑，参考http://stackoverflow.com/questions/19418427/comparing-strings-with-which-are-declared-final-in-java/19418517#19418517
+	 */
 	public void testEquals() {
 		String s1="ab";
 		String s2="c";
+		//变量创建对象在运时候创建，参考jsl 3.10.5 String Literals
 		String s3 = s1+s2;
 		
 		String s4="abc";
 		
+		//final类型的字符串常量是不可变的，在编译时就赋值了
 		final String ss1="ab";
-		final String ss2="c";
+		final String ss2="cd";
 		String ss = ss1+ss2;
-		System.out.println(ss1+ss2);
-		System.out.println(s3==s4);
-		System.out.println((ss1+ss2)=="abc");
+		System.out.println(ss1+ss2);//abc
+		System.out.println(s3==s4);//false
+		System.out.println(s3.intern()==s4.intern());//当s3.equals(s4)为true时，此表达式才返回true
+		System.out.println((ss1+ss2)=="abcd");//true，参考4.12.4. final Variables，
+		System.out.println(ss=="abcd");
+		System.out.println((as+bs)==s1);
 	}
 	
 	
@@ -111,7 +124,9 @@ public class TestString {
 		System.out.println("tostring:" + price.movePointRight(2).toString());
 	}
 	
-	
+	/**
+	 * 注意Sting有几个方法，传参用的正则表达式，如：replaceAll
+	 */
 	public void testRex() {
 		String s = "20100102377892";
 		System.out.println(s.replaceAll("\\s", "','"));
@@ -120,6 +135,10 @@ public class TestString {
 		String[] array = orders.split("\\s");
 		List<String> list = Arrays.asList(array);
 		System.out.println(list.size());
+		
+		String slash = "sajfl\\sdf";
+		//TODO未替换成功(搞明白\转义，参考Pattern api文档，以及规范)
+		System.out.println(slash.replaceAll("\\\\", "\\\\"));
 	}
 	
 	public void tString(String s) {
