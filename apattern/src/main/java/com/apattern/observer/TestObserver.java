222e646c0444c283c447dd85bb142a6f628e686f
dukezeng <zenghua233@163.com>
2014-05-20 00:13:14 +0800

    Observer

--- apattern/src/main/java/com/apattern/observer/TestObserver.java
+++ apattern/src/main/java/com/apattern/observer/TestObserver.java
@@ -14,8 +14,13 @@ public class TestObserver {
 	public static void main(String args[]) {
 		TestObserver t = new TestObserver();
 		t.testProduct();
+//		t.testASubject();
+//		t.testBSubject();
 	}
 	
+	/**
+	 * JDK自带的观察者模式
+	 */
 	public void testProduct() {
 		Product product = new Product();
 		ProductNameObserver nameobs = new ProductNameObserver();
@@ -26,5 +31,26 @@ public class TestObserver {
 		product.setName("橘子红了");
 		product.setName("橘子红了");
 		product.setPrice(9.22f);
+		product.setPrice(100.0f);
+	}
+	
+	/**
+	 * 被观察者的操作由具体类去实现
+	 */
+	public void testASubject() {
+		ASubject subject = new AConcreteSubject();
+		AObserver observer = new AConcreteObserver();
+		subject.add(observer);
+		subject.notifyObservers();
+	}
+	
+	/**
+	 * 被观察者的具体实现有抽象类去实现
+	 */
+	public void testBSubject() {
+		BConcreteSubject subject = new BConcreteSubject();
+		BObserver observer = new BConcreteObserver();
+		subject.add(observer);
+		subject.change("newstate");
 	}
 }
