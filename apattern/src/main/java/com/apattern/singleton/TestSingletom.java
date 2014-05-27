0ac75c4e657257f057a02e4f566c84cb0e45e269
dukezeng <zenghua233@163.com>
2014-05-27 16:35:59 +0800

    init

---
+++ apattern/src/main/java/com/apattern/singleton/TestSingletom.java
@@ -0,0 +1,15 @@
+package com.apattern.singleton;
+
+/**
+ * @description: TODO
+ *
+ * @createtime: 2014-5-27 下午4:35:24
+ *
+ * @author zenghua
+ *
+ * @version 1.0
+ */
+
+public class TestSingletom {
+
+}
