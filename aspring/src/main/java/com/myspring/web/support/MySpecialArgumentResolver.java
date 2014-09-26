af318f19fcaf70aac69cdc9ce16497c8b136c70d
dukezeng <zenghua233@163.com>
2014-09-26 18:52:29 +0800

    tag

--- aspring/src/main/java/com/myspring/web/support/MySpecialArgumentResolver.java
+++ aspring/src/main/java/com/myspring/web/support/MySpecialArgumentResolver.java
@@ -4,7 +4,7 @@ import org.springframework.core.MethodParameter;
 import org.springframework.web.bind.support.WebArgumentResolver;
 import org.springframework.web.context.request.NativeWebRequest;
 
-import com.myspring.web.support.annotation.MyInject;
+import com.myspring.web.tag.pagination.Page;
 
 public class MySpecialArgumentResolver implements WebArgumentResolver
 {
@@ -13,7 +13,9 @@ public class MySpecialArgumentResolver implements WebArgumentResolver
 	public Object resolveArgument(MethodParameter methodParameter,
 			NativeWebRequest webRequest) throws Exception
 	{
-		if (methodParameter.getParameterType().equals(MyInject.class)) {
+		System.out.println(methodParameter.getParameterType());
+//		methodParameter.getParameterAnnotation(MyInject.class);
+		if (methodParameter.getParameterType().equals(Page.class)) {
 			//获取参数并给获取查询后的Page
 			System.out.println("myinject");
 		}
--- aspring/src/main/java/com/myspring/web/tag/controller/TagController.java
+++ aspring/src/main/java/com/myspring/web/tag/controller/TagController.java
@@ -4,13 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.myspring.web.tag.pagination.Page;
+
 @Controller
 @RequestMapping(value = "/tag")
 public class TagController
 {
 	@RequestMapping(value = "/page")
-	public ModelAndView page() {
-		ModelAndView mv = new ModelAndView("/tag/pageTag.jsp");
+	public ModelAndView page(Page page) {
+		ModelAndView mv = new ModelAndView("tag/pageTag");
 		return mv;
 	}
 }
