298404cb916b2f66c03ef9bb7464cd50527f41cc
dukezeng <zenghua233@163.com>
2015-05-08 17:07:24 +0800

    包整理

--- ajse/src/main/java/com/jse/Test.java
+++ ajse/src/main/java/com/jse/Test.java
@@ -1,10 +1,14 @@
 package com.jse;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+
+import com.alibaba.fastjson.JSON;
 
 /**
  * @description: TODO
@@ -22,16 +26,27 @@ public class Test {
 	
 	public static void main(String[] args) {
 		Test t = new Test();
-		List<String> list = null;
-		CollectionUtils.isNotEmpty(list);
 		
-		t.test1(t);
-		System.out.println(t.getSize());
-		log.info("testddddddddddddddddd");
+		String s = "1";
+//		System.out.println(s+ 1L);
+//		List<String> list = null;
+//		CollectionUtils.isNotEmpty(list);
+//		
+//		t.test1(t);
+//		System.out.println(t.getSize());
+//		log.info("testddddddddddddddddd");
+		
+//		t.convert();
+//		t.jsonMap();
+		
+		System.out.println(Long.valueOf(null));
 	}
 	
-	public void test1(Test t) {
+	public void test1(Test t) 
+	{
 		t.test(size++);
+		Map<String, Object> map = new HashMap<String, Object>();
+		System.out.println(map);
 	}
 	
 	public void test(int size) {
@@ -39,5 +54,18 @@ public class Test {
 	
 	public int getSize() {
 		return size;
+	}
+	
+	public void convert() {
+		List<Long> list = new ArrayList<Long>();
+//		list.add(1L);
+		System.out.println(list.toArray(new Long[0]).length);
+	}
+	
+	public void jsonMap() {
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("sessionId", "123456");
+		map.put("entry", new Test());
+		System.out.println(JSON.toJSONString(map));
 	}
 }
