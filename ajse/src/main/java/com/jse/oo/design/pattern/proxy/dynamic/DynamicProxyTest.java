83757908342dde4f8c2356aa0fddfa4143a37760
dukezeng <zenghua233@163.com>
2015-12-09 08:35:50 +0800

    动态代理模式测试

--- ajse/src/main/java/com/jse/oo/design/pattern/proxy/dynamic/DynamicProxyTest.java
+++ ajse/src/main/java/com/jse/oo/design/pattern/proxy/dynamic/DynamicProxyTest.java
@@ -2,11 +2,19 @@ package com.jse.oo.design.pattern.proxy.dynamic;
 
 import java.lang.reflect.Proxy;
 
-public class DynamicProxyTest
-{
-    public static void main(String[] args)
-    {
-       Woman woman = (Woman) Proxy.newProxyInstance(Woman.class.getClassLoader(), new Class[] {Woman.class}, new OldWoman());
-       
-    }
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
+
+/**
+ * 动态代理：就是在运行时创建代理对象
+ * 
+ * @author Administrator
+ *
+ */
+public class DynamicProxyTest {
+	public static void main(String[] args) {
+		YoungWoman youngWoman = new YoungWoman();
+		OldWoman oldWoman = new OldWoman(youngWoman);
+		Woman woman = (Woman) Proxy.newProxyInstance(Woman.class.getClassLoader(), new Class[] { Woman.class }, oldWoman);
+		woman.doHappy();
+	}
 }
--- ajse/src/main/java/com/jse/oo/design/pattern/proxy/dynamic/OldWoman.java
+++ ajse/src/main/java/com/jse/oo/design/pattern/proxy/dynamic/OldWoman.java
@@ -3,14 +3,26 @@ package com.jse.oo.design.pattern.proxy.dynamic;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 
+/**
+ * 代理类
+ * @author Administrator
+ *
+ */
 public class OldWoman implements InvocationHandler
 {
-    public YoungWoman yongWoman;
-
+	//真实对象
+    public YoungWoman youngWoman;
+    
+    public OldWoman(YoungWoman youngWoman) {
+    	this.youngWoman = youngWoman;
+    }
+    
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable
     {
-        return method.invoke(this, args);
+    	System.out.println("this is invoke method");
+//        return method.invoke(this, args);
+        return null;
     }
 
 }
